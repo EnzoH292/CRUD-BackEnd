d021d994ddeedaa0862805bc30af15a9852a1e3a
Enzo A. Heredia <eaheredia292@gmail.com>
2022-11-03 00:59:44 -0300

    agregamos la logica para crear un producto y obtener el arreglo de todos los productos

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -1,12 +1,39 @@
+import Producto from "../models/producto"
 
-export const listarProductos = (req, res)=>{
-    res.send('Aquí tengo que retornar un arreglo')
-}
+export const listarProductos = async (req, res)=>{
+    try {
+ 
+    //Buscar todos los productos en la base de datos
+    const listaProductos = await Producto.find();
 
-export const crearProductos = (req, res)=>{
-    //extraer del body los datos
+    //responder al usuario que todo salio bien
+    res.status(200).json(listaProductos)
+    } catch (error) {
+       console.log(error);
+       res.status(404).json({
+           mensaje: 'Error al intentar buscar los productos'
+       }); 
+    }
+};
+
+export const crearProductos = async (req, res)=>{
+   
+    try {
+        //extraer del body los datos
     console.log(req.body)
     // agregar la validacion correspondiente
+
+    const productoNuevo = new Producto(req.body);
     //guardar ese producto en la base de datos
-    res.send('Esto es una prueba de la peticion Post')
-}
+    await productoNuevo.save();
+    //responder al usuario que todo salio bien
+    res.status(201).json({
+        mesaje:'El producto fue correctamente creado'
+    }); 
+    } catch (error) {
+       console.log(error);
+       res.status(400).json({
+           mensaje: 'Error al intentar agregar un producto'
+       }); 
+    }
+};
